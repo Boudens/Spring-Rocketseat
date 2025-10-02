f80fd7f328ca6d541ee1e32b0aeb685ba89fefa5
Boudens <caioboudens00@gmail.com>
2025-10-02 16:58:44 -0300

    Atualização da validação do idUser

--- src/main/java/br/com/caioboudens/todolist/task/TaskController.java
+++ src/main/java/br/com/caioboudens/todolist/task/TaskController.java
@@ -1,27 +1,27 @@
 package br.com.caioboudens.todolist.task;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
 
 @RestController
 @RequestMapping("/tasks")
-
-
 public class TaskController {
 
     @Autowired
     private ITaskRepository taskRepository;
 
-    @PostMapping("/")
-    
-    public TaskModel create(@RequestBody TaskModel taskModel){
+    @PostMapping("/")    
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     }
